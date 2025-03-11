26b0282a88a29f1c70daf36767d71538cdbd2bc6
Eduardo Aguilar <es123y123@gmail.com>
2025-03-11 09:47:23 -0500

    c

--- src/main/java/com/icm/security_scorpion_api/controllers/DeviceGroupController.java
+++ src/main/java/com/icm/security_scorpion_api/controllers/DeviceGroupController.java
@@ -11,7 +11,6 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
